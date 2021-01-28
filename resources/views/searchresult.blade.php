19f8abcf65d4b832c005bb1176a310e43c0a830e
Tony <nput68@gmail.com>
2021-01-28 14:36:46 +0700

    fix search display

--- resources/views/searchresult.blade.php
+++ resources/views/searchresult.blade.php
@@ -12,21 +12,21 @@
         <thead class="black white-text">
             <tr>
                 <th scope="col" width="5%">#</th>
+                <th scope="col" width="10%">วันที่ใช้จ่าย</th>
                 <th scope="col" width="15%">ประเภท</th>
                 <th scope="col" width="20%">ชื่อรายการ</th>
                 <th scope="col" width="15%">จำนวนเงิน</th>
-                <th scope="col" width="10%">วันที่ใช้จ่าย</th>
                 <th scope="col" width="15%">action</th>
             </tr>
         </thead>
         <tbody>
-            @foreach($students as $student)
+            @foreach($students as $index => $student)
             <tr>
-                <th scope="row">{{ $student->id }}</th>
+                <th scope="row">{{ $index+1 }}</th>
+                <td>{{ $student->TransactionDate }}</td>
                 <td>{{ $student->type}}</td>
                 <td>{{ $student->TransactionName }}</td>
                 <td>{{ $student->Amount }}</td>
-                <td>{{ $student->TransactionDate }}</td>
                 <td>
                     <a href="{{ route('edit', $student->id) }}" class="btn btn-warning btn-sm">edit</a>
 
